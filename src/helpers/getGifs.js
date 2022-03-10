266bcdeb2dafe32eb950cbe4c2e19630bae224f7
jeasoncues <jeasoncues@gmail.com>
2022-03-10 13:47:51 -0500

    https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async ( categories ) => {
 
-    const url  = `http://api.giphy.com/v1/gifs/search?q=${encodeURI( categories )}&limit=10&api_key=mZviDqjHs3fNVUCAHwJ2J32tzw0e2kmp`;
+    const url  = `https://api.giphy.com/v1/gifs/search?q=${encodeURI( categories )}&limit=10&api_key=mZviDqjHs3fNVUCAHwJ2J32tzw0e2kmp`;
     const resp =  await fetch( url );
     const { data }  =  await resp.json();
 
